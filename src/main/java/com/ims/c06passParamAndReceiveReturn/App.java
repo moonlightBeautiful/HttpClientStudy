930bf9c43c49d53215356035b18b1b49e4d7e176
高旭 <xy20539@163.com>
2020-06-02 14:26:17 +0800

    http get/post 携带参数

--- src/main/java/com/ims/c06passParamAndReceiveReturn/App.java
+++ src/main/java/com/ims/c06passParamAndReceiveReturn/App.java
@@ -1,16 +1,20 @@
 package com.ims.c06passParamAndReceiveReturn;
 
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,34 +25,39 @@ import java.util.List;
  */
 public class App {
 
-    public static void main(String[] args) throws IOException {
-
-
-        // get方式的传统传值
-       /* HttpGet get = new HttpGet("http://localhost:8080/HttpClientStudy/c06passParamAndReceiveReturn/test.jsp?name=jiaxin");
-        get.setHeader("Accept","application/json;charset=utf-8");
-        HttpResponse response = client.execute(get);
-        System.out.println("响应码："+response.getStatusLine());
-        System.out.println("响应body体："+ EntityUtils.toString(response.getEntity()));*/
-
-        //post方式的传统传值
-        /*HttpPost post = new HttpPost("http://localhost:8080/HttpClientStudy/c06passParamAndReceiveReturn/test.jsp");
-        post.setHeader("Accept", "application/json;charset=utf-8");
-        JSONObject params = new JSONObject();
-        params.put("name", "zhangSan");
-        // 传值时传递的是json字符串，这样的好处是在服务端无需建立参数模型，直接接收String，便于后期维护。
-        StringEntity stringEntity = new StringEntity(params.toString(), "utf-8");
-        post.setEntity(stringEntity);
-        HttpResponse response = client.execute(post);
-        System.out.println("响应码：" + response.getStatusLine());
-        System.out.println("响应body体：" + EntityUtils.toString(response.getEntity()));*/
-
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
         //form方式
+        //post
+        /*
+         * 由于GET请求的参数都是拼装在URL地址后方，所以我们要构建一个URL，带参数
+         */
+        URIBuilder uriBuilder = new URIBuilder("http://localhost:8080/HttpClientStudy/c06passParamAndReceiveReturn/test.jsp");
+        /** 第一种添加参数的形式 */
+        /*uriBuilder.addParameter("name", "root");
+        uriBuilder.addParameter("password", "123456");*/
+        /** 第二种添加参数的形式 */
+        List<NameValuePair> list = new LinkedList<>();
+        BasicNameValuePair param1 = new BasicNameValuePair("name", "root");
+        BasicNameValuePair param2 = new BasicNameValuePair("password", "123456");
+        list.add(param1);
+        list.add(param2);
+        uriBuilder.setParameters(list);
+        /** 第三种 直接放在url后面 */
+        /*HttpGet get = new HttpGet("http://localhost:8080/HttpClientStudy/c06passParamAndReceiveReturn/test.jsp?name=jiaxin");*/
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(uriBuilder.build());
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.6)");
+        httpGet.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        HttpResponse response = client.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        System.out.println("响应码：" + response.getStatusLine());
+        System.out.println("响应body体：" + EntityUtils.toString(response.getEntity()));
 
         //post
         // 创建请求参数
-        CloseableHttpClient client = HttpClients.createDefault();
+        /*CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://localhost:8080/HttpClientStudy/c06passParamAndReceiveReturn/test.jsp");
         httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.6)");
         httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -61,6 +70,6 @@ public class App {
         httpPost.setEntity(entityParam);
         HttpResponse response = client.execute(httpPost);
         System.out.println("响应码：" + response.getStatusLine());
-        System.out.println("响应body体：" + EntityUtils.toString(response.getEntity()));
+        System.out.println("响应body体：" + EntityUtils.toString(response.getEntity()));*/
     }
 }
